0fc23062483570622c4039fd74e27724721ece15
arrietybeu <arrietybeu@gmail.com>
2025-08-04 06:02:59 +0700

    <update> load grid view giftcode

--- KnightAgeTool/src/Giftcode.Designer.cs
+++ KnightAgeTool/src/Giftcode.Designer.cs
@@ -33,8 +33,24 @@
             label3 = new Label();
             pictureBox2 = new PictureBox();
             groupBox1 = new GroupBox();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            Column8 = new DataGridViewTextBoxColumn();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -73,19 +89,158 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(dataGridView1);
             groupBox1.ForeColor = Color.Silver;
             groupBox1.Location = new Point(12, 36);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1122, 670);
+            groupBox1.Size = new Size(1122, 445);
             groupBox1.TabIndex = 7;
             groupBox1.TabStop = false;
             groupBox1.Text = "Giftcode";
+            // 
+            // button6
+            // 
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Location = new Point(998, 158);
+            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(86, 31);
+            button6.TabIndex = 9;
+            button6.Text = "DELETE";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(998, 197);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(86, 31);
+            button5.TabIndex = 8;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(998, 119);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(86, 31);
+            button4.TabIndex = 7;
+            button4.Text = "ADD";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(998, 77);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(86, 31);
+            button3.TabIndex = 6;
+            button3.Text = "EDIT";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(998, 38);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 31);
+            button2.TabIndex = 3;
+            button2.Text = "SELECT";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8 });
+            dataGridView1.GridColor = SystemColors.Control;
+            dataGridView1.Location = new Point(20, 38);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(944, 382);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "ID";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "CODE";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Số lượng";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Start";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "End";
+            Column5.MinimumWidth = 6;
+            Column5.Name = "Column5";
+            Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            Column6.HeaderText = "Need active";
+            Column6.MinimumWidth = 6;
+            Column6.Name = "Column6";
+            Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            Column7.HeaderText = "Items_reward";
+            Column7.MinimumWidth = 6;
+            Column7.Name = "Column7";
+            Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            Column8.HeaderText = "Players entered";
+            Column8.MinimumWidth = 6;
+            Column8.Name = "Column8";
+            Column8.Width = 140;
             // 
             // Giftcode
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1146, 718);
+            ClientSize = new Size(1146, 500);
             Controls.Add(groupBox1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
@@ -95,6 +250,8 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -104,5 +261,19 @@
         private Label label3;
         private PictureBox pictureBox2;
         private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+        private Button button2;
+        private Button button4;
+        private Button button3;
+        private Button button5;
+        private Button button6;
     }
 }
--- KnightAgeTool/src/Giftcode.cs
+++ KnightAgeTool/src/Giftcode.cs
@@ -32,6 +32,7 @@ namespace KnightAgeTool.src
 
             panel1.MouseDown += new MouseEventHandler(panel1_MouseDown);
             label3.MouseDown += new MouseEventHandler(panel1_MouseDown);
+            dataGridView1.EnableHeadersVisualStyles = false;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -44,11 +45,28 @@ namespace KnightAgeTool.src
 
             this.BackColor = System.Drawing.Color.FromArgb(45, 45, 48);
             panel1.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);// rgb(37, 37, 38)
-            //button1.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
-            //button2.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
-            //button3.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+            button2.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+            button3.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+            button4.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+            button5.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+
             groupBox1.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
-            //comboBox1.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(45, 45, 48);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(60, 60, 64);
+
+            // Dòng dữ liệu
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.FromArgb(37, 37, 38);
+            dataGridView1.RowsDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle.SelectionBackColor = Color.FromArgb(70, 70, 75);
+            dataGridView1.RowsDefaultCellStyle.SelectionForeColor = Color.White;
+
+            // Border & grid
+            dataGridView1.GridColor = Color.FromArgb(55, 55, 60);
+            dataGridView1.BackgroundColor = Color.FromArgb(37, 37, 38);
+            dataGridView1.BorderStyle = BorderStyle.None;
+
             //HostBox.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
             //UserBox.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
             //PasswordBox.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
@@ -83,5 +101,33 @@ namespace KnightAgeTool.src
 
         private const int WM_NCLBUTTONDOWN = 0xA1;
         private const int HTCAPTION = 0x2;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT id, CODE, amount, start, end, need_active, items_reward, players_entered FROM gift_code";
+            DataTable table = database.ExecuteQuery(query);
+            //MessageBox.Show("Load thanh cong: " + database.GetDatabaseName());
+
+            dataGridView1.Rows.Clear();
+
+            foreach (DataRow row in table.Rows)
+            {
+                dataGridView1.Rows.Add(
+                    row["id"],
+                    row["CODE"],
+                    row["amount"],
+                    row["start"],
+                    row["end"],
+                    row["need_active"],
+                    row["items_reward"],
+                    row["players_entered"]
+                );
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
     }
 }
